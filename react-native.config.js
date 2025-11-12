cf7751058cb37eb42fdacbd57ddf9d3699a82a53
Britox7 <kauebritodev@gmail.com>
2025-11-12 10:42:20 -0300

    Notificações

---
+++ react-native.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  project: {
+    android: {},
+    ios: {},
+  },
+  assets: [],
+  dependencies: {
+    '@react-native-async-storage/async-storage': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    '@react-native-community/datetimepicker': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
+};
---
+++ utils/notificationsConfig.ts
@@ -0,0 +1,32 @@
+// utils/notificationsConfig.ts
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+import { Platform } from 'react-native';
+
+export async function registerForPushNotificationsAsync() {
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+
+    if (finalStatus !== 'granted') {
+      alert('Permissão para notificações não concedida!');
+      return;
+    }
+  } else {
+    alert('As notificações só funcionam em dispositivos físicos.');
+  }
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.HIGH,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+}
